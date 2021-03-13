714d0d5d94afc3620b2a1d55865417b8143a3c55
Randell Anderson <zugzug07@gmail.com>
2021-03-13 16:23:10 -0600

    Day 4 of project

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(function() {
+    $('#picO').click(function() {
+        $('#olympic').collapse('show');
+    });
+    $('#picO').click(function() {
+        $('#olympic').collapse('hide');
+    });
+
+    $('#picT').click(function() {
+        $('#titanic').collapse('show');
+    });
+    $('#picT').click(function() {
+        $('#titanic').collapse('hide');
+    });
+
+    $('#picB').click(function() {
+        $('#britannic').collapse('show');
+    });
+    $('#picB').click(function() {
+        $('#britannic').collapse('hide');
+    });
+
+
+});
